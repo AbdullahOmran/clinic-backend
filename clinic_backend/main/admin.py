b11b0334a4321f3e6156363520ab73e80b972ae8
AbdullahOmran <abdullahomrana12312332110h@gmail.com>
2024-02-13 17:34:55 +0200

    updated

--- clinic_backend/main/admin.py
+++ clinic_backend/main/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from .models import Doctor, Patient,Secretary
 
+class DoctorAdmin(ModelAdmin):
+    list_display = ["id",'username', "age"]
 
-admin.site.register(Doctor)
+    @admin.display(empty_value="???")
+    def username(self, obj):
+        return obj.user.username
+
+admin.site.register(Doctor,DoctorAdmin)
 admin.site.register(Patient)
 admin.site.register(Secretary)
 # Register your models here.

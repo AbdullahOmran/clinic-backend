3865d39ca44786659e19c2b3003fd78f2c6df2a4
AbdullahOmran <abdullahomrana12312332110h@gmail.com>
2024-04-16 22:06:27 +0200

    updated

--- clinic_backend/main/api/urls.py
+++ clinic_backend/main/api/urls.py
@@ -16,7 +16,6 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 urlpatterns = [
-    # path('', schema_view),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
     # path('', views.getRoutes),
     path('token/', MyTokenObtainPairView.as_view(), name = 'token_obtain_pair'),

ed4650b26937eed68c6b87d22d236684e5de4225
AbdullahOmran <abdullahomrana12312332110h@gmail.com>
2024-04-16 22:02:24 +0200

    updated

--- clinic_backend/clinic_backend/urls.py
+++ clinic_backend/clinic_backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('main.api.urls')),
+    # path('docs/', include('rest_framework_swagger.urls')),
 ]
--- clinic_backend/main/api/urls.py
+++ clinic_backend/main/api/urls.py
@@ -3,9 +3,22 @@ from . import views
 from rest_framework_simplejwt.views import TokenRefreshView 
 from .views import MyTokenObtainPairView
 
+from rest_framework_swagger.views import get_swagger_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Episyche Technologies",
+        default_version='v1',),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
-    path('', views.getRoutes),
+    # path('', schema_view),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
+    # path('', views.getRoutes),
     path('token/', MyTokenObtainPairView.as_view(), name = 'token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name = 'token_refresh'),
     path('user/',views.user_details),
